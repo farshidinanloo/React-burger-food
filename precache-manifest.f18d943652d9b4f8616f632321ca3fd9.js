7c07d7568779ad828d4f6a320d6120639af2ece5
farshid inanloo <farshidinanloo73@gmail.com>
2019-09-02 23:05:21 +0430

    Updates

---
+++ precache-manifest.f18d943652d9b4f8616f632321ca3fd9.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f21e459ac5d8086f951bfb6fe8e1d688",
+    "url": "/React-Burger-Food/index.html"
+  },
+  {
+    "revision": "8fa2e7a06f575d043b60",
+    "url": "/React-Burger-Food/static/css/3.dd641f77.chunk.css"
+  },
+  {
+    "revision": "97c9f322480550178e68",
+    "url": "/React-Burger-Food/static/css/4.e35679fe.chunk.css"
+  },
+  {
+    "revision": "d95d3b60aa7baea51086",
+    "url": "/React-Burger-Food/static/css/5.35ec6585.chunk.css"
+  },
+  {
+    "revision": "44498014c6a9a797fe30",
+    "url": "/React-Burger-Food/static/css/main.93f4b65a.chunk.css"
+  },
+  {
+    "revision": "4882b56fcbfae16a3720",
+    "url": "/React-Burger-Food/static/js/2.4a7b63fa.chunk.js"
+  },
+  {
+    "revision": "8fa2e7a06f575d043b60",
+    "url": "/React-Burger-Food/static/js/3.ecd6f8e5.chunk.js"
+  },
+  {
+    "revision": "97c9f322480550178e68",
+    "url": "/React-Burger-Food/static/js/4.bd8e32b2.chunk.js"
+  },
+  {
+    "revision": "d95d3b60aa7baea51086",
+    "url": "/React-Burger-Food/static/js/5.8061eda5.chunk.js"
+  },
+  {
+    "revision": "44498014c6a9a797fe30",
+    "url": "/React-Burger-Food/static/js/main.91e2b575.chunk.js"
+  },
+  {
+    "revision": "0493a88f6930e77e2d01",
+    "url": "/React-Burger-Food/static/js/runtime~main.6289cf3b.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/React-Burger-Food/static/media/burger-logo.b8503d26.png"
+  }
+]);
